22af20e93aa062e3ebc85aafdd56f1212c78d86c
placidowang <placidow@gmail.com>
2020-02-17 09:57:04 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Placido."
+    end
+
+    get '/hometown' do
+        "My hometown is Houston."
+    end
+
+    def say_favorite_song
+        "My favorite song is Baby Shark."
+    end
+
+    get '/favorite-song' do
+        say_favorite_song
+    end
 end
